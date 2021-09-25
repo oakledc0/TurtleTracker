57d9794f215aa4355dfa52574c21e326f6bd481e
Cal Oakley <daniel.oakley@duke.edu>
2021-09-25 16:09:36 -0400

    Handling Errors with if statements

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -50,6 +50,7 @@ for lineString in line_list:
         date_dict[record_id] = obs_date
         coord_dict[record_id] = (obs_lat, obs_lon)
 
+#reverse lookup line
 user_date = input("Enter a date (M/D/YYYY):")
 
 #Create an empty key list
@@ -61,10 +62,15 @@ for the_key, the_value in date_dict.items():
     if the_value == user_date:
         matching_keys.append(the_key)
 
-#Reveal locations for each key in matching_keys
-for matching_key in matching_keys:
-    obs_lat, obs_long = coord_dict[matching_key]
-    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {user_date}")
+#Check that at least one key was returned; tell the user if not
+if len(matching_keys) == 0:
+    print("No observations recorded for {}".format(user_date))
+else:
+
+            #Reveal locations for each key in matching_keys
+    for matching_key in matching_keys:
+        obs_lat, obs_long = coord_dict[matching_key]
+        print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {user_date}")
     
 
 # Close the file
